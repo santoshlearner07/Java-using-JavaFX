fa319f844d939d8d92c0fb46d418a6347f478d1d
Santosh Nandiyawar <nandiyawarsantosh.0719@gmail.com>
2024-02-19 01:22:52 +0530

    Created:- Second button but overlapping

--- Main.java
+++ Main.java
@@ -1,29 +1,43 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main extends Application{
 
     Button button;
+    Button button2;
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage){
         primaryStage.setTitle("Title of Window");
 
         button = new Button();
-        button.setText("Click me");
+        button2 = new Button();
+        button.setText("Button Name");
+        button2.setText("Button Name 2 ");
+        button.setOnAction(e->{
+            System.out.println("Hey SetOnAction");
+            System.out.println("Hello world");
+        });
+    
 
         StackPane layout = new StackPane();
+
         layout.getChildren().add(button);
+        layout.getChildren().add(button2);
+
 
         Scene scene = new Scene(layout, 300, 250);
         primaryStage.setScene(scene);
+
 
         primaryStage.show();
 
